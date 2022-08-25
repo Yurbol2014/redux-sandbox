9b1efe414df9bb695debd87aa222e16bef96f754
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-08-26 00:43:16 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const reducer = (state = 0, action)=>{
+   switch(action.type){
+      case 'INC':
+         return state + 1;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+         default:
+            return state;
+   }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let state = reducer(undefined,{});
+
+state =  reducer(state, {type: 'INC'});
+console.log(state);
+
+state =  reducer(state, {type: 'INC'});
+console.log(state);

6e9b5ca522fed22f645aec71be7be5406f94ec7e
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-09-02 00:10:35 +0300

    React и Redux

--- src/counter.js
+++ src/counter.js
@@ -1,14 +1,20 @@
-import React from 'react-scripts';
+import React from 'react';
 
 
-const Counter = () => {
+const Counter = ({counter, inc, dec, rnd}) => {
 
    return (
       <div  className="jumbotron">
-      <h2>0</h2>
-      <button  className="btn btn-primary btn-lg">DEC</button>
-      <button className="btn btn-primary btn-lg">INC</button>
-      <button className="btn btn-primary btn-lg">RND</button>
+      <h2>{counter}</h2>
+      <button  
+      onClick={dec}
+      className="btn btn-primary btn-lg">DEC</button>
+      <button 
+      onClick={inc}
+      className="btn btn-primary btn-lg">INC</button>
+      <button 
+      onClick={rnd}
+      className="btn btn-primary btn-lg">RND</button>
      </div>
    )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import  ReactDOM  from 'react-dom';
 import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
 import * as actions  from './actions'
+import Counter from './counter';
 
 
 
@@ -12,33 +14,23 @@ const {dispatch} = store;
 
 const {inc,dec,rnd} = bindActionCreators(actions,dispatch);
 
-//document
-//.getElementById('inc')
-//.addEventListener('click', inc);
 
-//document
-//.getElementById('dec')
-//.addEventListener('click', dec); // ЭТО ПРИМЕР РАБОТЫ БЕЗ RECTA
-//document
-//.getElementById('rnd')
-//.addEventListener('click', () => {
-//   const payload = Math.floor(Math.random()*10);
-//   rnd(payload);
-//});
 
 const update =() => {
-   //document
-   //.getElementById('counter')
-   //.innerHTML = store.getState();// ЭТО ПРИМЕР РАБОТЫ БЕЗ RECTA
+   ReactDOM.render(<Counter
+      counter={store.getState()}
+     inc={inc}
+     dec={dec}
+     rnd={()=>{
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+     }}/>, document.getElementById('root'))
 };
+ update();
  store.subscribe(update);
 
-//let state = reducer(undefined,{});
 
-//state =  reducer(state, {type: 'INC'}); //Было до подключения Redux
-//console.log(state);
 
-//state =  reducer(state, {type: 'INC'});
-//console.log(state);
+
 
 
